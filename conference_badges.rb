6317f2d02d423ba4d553d2e2671f60f06c4ff266
Tony Ho <ho.tonym@gmail.com>
2018-05-07 08:33:08 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,6 +14,7 @@ end
 def assign_rooms(array)
   new_array = Array.new
   for speaker in array
+    puts "Hello, #{speaker}! You'll be assigned to room #{array.index(speaker)+1}!"
     new_array << "Hello, #{speaker}! You'll be assigned to room #{array.index(speaker)+1}!"
   end
   return new_array
