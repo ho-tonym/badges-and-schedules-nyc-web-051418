efed375d71a65b615bcec3d55c1bcac8188bc027
Tony Ho <ho.tonym@gmail.com>
2018-05-07 08:10:52 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  new_array = Array.new
+  for element in array
+    new_array << badge_maker(element)
+  end
+end
+
+def assign_rooms(speakers)
+  new_array = Array.new
+  for speaker in speakers
+    new_array << "Hello, #{speaker}! You'll be assigned to room #{speakers.index(speaker)}!"
+    
+  end
+end
+
+def printer(array)
+  for element in array
+    batch_badge_creator(array)
+    assign_rooms(array)
+  end
+end
